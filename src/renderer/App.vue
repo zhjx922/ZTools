c4dc707ac4118f8c00152fd00ec8efee4aab18c1
zhaojingxian <zhaojingxian@51talk.com>
2019-04-17 22:57:57 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ztools'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
